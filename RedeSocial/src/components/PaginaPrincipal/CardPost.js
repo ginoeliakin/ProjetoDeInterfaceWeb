a3a71cc518c2706a957e422111a2b1eb1b6f8566
ginoeliakin <40217815+ginoeliakin@users.noreply.github.com>
2020-04-14 15:54:10 -0300

    .

--- RedeSocial/src/components/PaginaPrincipal/CardPost.js
+++ RedeSocial/src/components/PaginaPrincipal/CardPost.js
@@ -2,12 +2,26 @@ import React, { Component } from 'react';
 import './CardPost.css'
 
  export default class CardPost extends Component {
-    state = {  }
+    state = {
+        curtidas:0,
+    }
+
+    incrementarCurtidas=() =>{
+        this.setState({curtidas: this.state.curtidas+1})
+    }
+
+    curtiu=() =>{
+        this.incrementarCurtidas()
+        this.props.curtiu(this.state.curtidas)
+    }
+
     render() { 
         return ( 
             <div className="card">
-                <h3>{this.props.nome}</h3>
-                <span>{this.props.post}</span>
+                <h3>{this.props.nome}</h3><br></br>
+                <span>{this.props.post}</span><br></br>
+                <br></br>
+                <button onClick= {this.curtiu}>Curtir</button>
             </div>
         );
     }
--- RedeSocial/src/components/PaginaPrincipal/Galeria.js
+++ RedeSocial/src/components/PaginaPrincipal/Galeria.js
@@ -3,20 +3,57 @@ import CardPost from './CardPost';
 import './Galeria.css'
 
 
-class Galeria extends Component {
-    state = { }
+export default class Galeria extends Component {
+    state = {posts:[
+        {
+            nome:"Harry Potter",
+            post:"Eu não saio por aí procurando encrenca. Em geral as encrencas é que vêm ao meu encontro.",
+        },
+
+        {
+            nome:"Chris",
+            post:"Sacomé né?",
+        },
+
+        {
+            nome:"Alvo Percival Wulfrico Brian Dumbledore",
+            post:"Não vale a pena mergulhar nos sonhos e esquecer de viver.",
+        },
+
+        {
+            nome:"Semagol",
+            post:"My precious",
+        },
+
+        {
+            nome:"Edward Eric",
+            post:"Sempre existem coisas mais importantes do que sonhos"
+        }
+    ]}
+
+    foiCurtido= (curtidas) => {
+        console.log("Seu post foi curtido" + curtidas + "vezes")
+    }
+
+
     render() { 
         return ( 
             
             <div className = 'Galeria'>              
                                 
-                <CardPost nome="João e Maria" post="Um Bom dia para caçar bruxas."></CardPost>
+                {/* <CardPost nome="João e Maria" post="Um Bom dia para caçar bruxas."></CardPost>
                 <CardPost nome="Harry Potter" post="Wingardium Leviosa"></CardPost>
                 <CardPost nome="Chris" post="Sacomé né?"></CardPost>
                 <CardPost nome="Alvo Percival Wulfrico Brian Dubledore" post="De que adianta viver sonhando e esquecer de viver?"></CardPost>
                 <CardPost nome="Smeagol" post="My precious"></CardPost>
                 <CardPost nome="Edward Eric" post="Sempre existem coisas mais importantes do que sonhos"></CardPost>
-                    
+                     */}
+                {this.state.posts.map(
+                    (postagem) => (<CardPost
+                    nome={postagem.nome}
+                    post={postagem.post}
+                    curtiu={this.foiCurtido}></CardPost>)
+                )}
 
                 
                 
@@ -26,4 +63,3 @@ class Galeria extends Component {
     }
 }
  
-export default Galeria;
